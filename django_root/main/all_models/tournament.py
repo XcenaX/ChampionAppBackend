4a6af466057aaaa92028f7ca16fd9d9739e2c153
XcenaX <vlad-057@maul.ru>
2024-04-29 22:46:34 +0500

    added fields to tournament

--- django_root/main/all_models/tournament.py
+++ django_root/main/all_models/tournament.py
@@ -16,18 +16,18 @@ class Tournament(models.Model):
     end = models.DateTimeField(verbose_name='Дата и время окончания турнира', blank=True, null=True, db_index=True)
     register_open_until = models.CharField(choices=REGISTER_OPEN_UNTIL, default="15 мин", max_length=50, verbose_name="Регистрация открыта до...")
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE, verbose_name='Вид спорта', db_index=True)
-    enter_price = models.IntegerField(verbose_name='Цена участия', db_index=True)
+    enter_price = models.IntegerField(verbose_name='Цена участия', db_index=True, validators=[MinValueValidator(0), MaxValueValidator(10000000)],)
     prize_pool = models.IntegerField(verbose_name='Призовой фонд')
-    first_place_prize = models.IntegerField(blank=True, null=True, verbose_name='Награда за 1 место')
-    second_place_prize = models.IntegerField(blank=True, null=True, verbose_name='Награда за 2 место')
-    third_place_prize = models.IntegerField(blank=True, null=True, verbose_name='Награда за 3 место')
+    first_place_prize = models.IntegerField(blank=True, null=True, verbose_name='Награда за 1 место', validators=[MinValueValidator(1), MaxValueValidator(10000000)],)
+    second_place_prize = models.IntegerField(blank=True, null=True, verbose_name='Награда за 2 место', validators=[MinValueValidator(1), MaxValueValidator(10000000)],)
+    third_place_prize = models.IntegerField(blank=True, null=True, verbose_name='Награда за 3 место', validators=[MinValueValidator(1), MaxValueValidator(10000000)],)
     rules = models.TextField(verbose_name='Правила', default="")
     users_requests = models.ManyToManyField(User, related_name='users_requests_tournament', verbose_name='Инливидуальные запросы на участие')
     teams_requests = models.ManyToManyField(Team, related_name='_teams_requests_tournament', verbose_name='Командные запросы на участие')
     moderators = models.ManyToManyField(User, related_name='moderators_tournament', verbose_name='Модераторы турнира')
     max_participants = models.IntegerField(default=4, verbose_name='Максимальное количество участников', validators=[MinValueValidator(4), MaxValueValidator(128)],)
-    max_team_size = models.IntegerField(blank=True, null=True, verbose_name='Максимальное количество участников в команде')
-    min_team_size = models.IntegerField(blank=True, null=True, verbose_name='Минимальное количество участников в команде')
+    max_team_size = models.IntegerField(blank=True, null=True, verbose_name='Максимальное количество участников в команде', validators=[MinValueValidator(2), MaxValueValidator(100)],)
+    min_team_size = models.IntegerField(blank=True, null=True, verbose_name='Минимальное количество участников в команде', validators=[MinValueValidator(2), MaxValueValidator(100)],)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     bracket = models.PositiveSmallIntegerField(choices=TOURNAMENT_BRACKET_TYPE, null=True, blank=True, verbose_name='Тип сетки турнира')
     tournament_type = models.PositiveSmallIntegerField(choices=TOURNAMENT_TYPE, null=True, blank=True, verbose_name='Тип турнира')  
@@ -37,26 +37,26 @@ class Tournament(models.Model):
     is_team_tournament = models.BooleanField(default=False, verbose_name='Командный турнир')
     active_stage_position = models.IntegerField(default=1, verbose_name='Позиция активного этапа')
     
-    BO_number = models.IntegerField(default=1, verbose_name='Количество встреч в матче')
-    group_stage_BO_number = models.IntegerField(default=1, verbose_name='Количество встреч в матче (Групповой этап)')
+    BO_number = models.IntegerField(default=1, verbose_name='Количество встреч в матче', validators=[MinValueValidator(1), MaxValueValidator(5)],)
+    group_stage_BO_number = models.IntegerField(default=1, verbose_name='Количество встреч в матче (Групповой этап)', validators=[MinValueValidator(1), MaxValueValidator(5)],)
 
     # Swiss
     win_points = models.FloatField(blank=True, null=True, verbose_name='Очки за победу')
     draw_points = models.FloatField(blank=True, null=True, verbose_name='Очки за ничью')
-    rounds_count = models.IntegerField(blank=True, null=True, verbose_name='Кол-во туров')
+    rounds_count = models.IntegerField(blank=True, null=True, verbose_name='Кол-во туров', validators=[MinValueValidator(1), MaxValueValidator(20)],)
 
     # Round Robin & Leaderboard
     check_score_difference_on_draw = models.BooleanField(default=False, verbose_name='Если очки равны. Первым смотреть разницу в очках')
     
     # Round Robin
-    mathces_count = models.IntegerField(blank=True, null=True, verbose_name='Количество игр с каждым игроком')
+    mathces_count = models.IntegerField(blank=True, null=True, verbose_name='Количество игр с каждым игроком', validators=[MinValueValidator(1), MaxValueValidator(5)],)
     
     # Двуступенчатый турнир
-    final_stage_advance_count = models.IntegerField(default=2, verbose_name='Количество команд которые проходят в плей-офф')
-    participants_in_group = models.IntegerField(default=2, verbose_name='Количество участников в группе')
+    final_stage_advance_count = models.IntegerField(default=2, verbose_name='Количество команд которые проходят в плей-офф', validators=[MinValueValidator(1), MaxValueValidator(128)],)
+    participants_in_group = models.IntegerField(default=2, verbose_name='Количество участников в группе', validators=[MinValueValidator(2), MaxValueValidator(64)],)
     group_stage_win_points = models.FloatField(blank=True, null=True, verbose_name='Очки за победу (Групповой этап)')
     group_stage_draw_points = models.FloatField(blank=True, null=True, verbose_name='Очки за ничью (Групповой этап)')
-    group_stage_rounds_count = models.IntegerField(blank=True, null=True, verbose_name='Кол-во туров (Групповой этап)')
+    group_stage_rounds_count = models.IntegerField(blank=True, null=True, verbose_name='Кол-во туров (Групповой этап)', validators=[MinValueValidator(1), MaxValueValidator(20)],)
 
 
     def get_active_stage(self):
